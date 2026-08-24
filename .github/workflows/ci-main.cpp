81aa7bd74c94f4c8cfe671f71df711b58f243d79
Fryy_55 <3pm.fryy@gmail.com>
2026-08-24 21:15:01 +0500

    Add CI testing

---
+++ .github/workflows/ci-main.cpp
@@ -0,0 +1,41 @@
+#include <aurora/aurora.hpp>
+
+
+namespace foo {
+
+template <typename ...Args>
+void trace(std::format_string<Args...> const& formatString, Args&&... args) noexcept {
+	using namespace aurora;
+
+	log::custom(
+		{
+			.logLevel=log::LogLevel::Debug,
+			.logLevelName="TRACE",
+			.headTag="\e[35m",
+			.bodyTag=log::LogLevel::Warn
+		},
+		formatString, std::forward<Args>(args)...
+	);
+
+	return;
+}
+
+} // namespace foo
+
+
+int main() {
+	using namespace aurora;
+
+	log::set12hTimeEnabled(true);
+	log::setFileLogLevel(log::LogLevel::Debug);
+
+	ThreadManager::get()->addThread("Main");
+
+	log::debug("[Cool Project] haii from Cool Project!");
+	log::info("[Project with a very long name] haii from Project with a very long name!");
+	log::warn("haii {} #{}!", "User", 43);
+	log::error("haii!");
+	foo::trace("haii {} #{}!", "User", 44);
+
+	return 0;
+}
